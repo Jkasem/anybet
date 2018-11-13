4ca05be81a8190261df04735039006627bc33212
Justin Kaseman <justinkaseman@live.com>
2018-11-13 12:13:30 -0800

    support for MetaMask change: no accounts exposed by default

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,19 @@ class App extends Component {
   state = { isConnected: false };
 
   componentWillMount() {
-    //Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof window.web3 !== "undefined") {
-      console.log("Found injected web3.");
-      // Use Mist/MetaMask's provider
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      try {
+        // Request account access if needed
+        window.ethereum.enable();
+        // Acccounts now exposed
+      } catch (error) {
+        // User denied account access...
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      // Acccounts always exposed
       this.web3 = new Web3(window.web3.currentProvider);
       this.web3.eth.net.getNetworkType().then(res => {
         if (res !== "ropsten") {
@@ -20,11 +29,11 @@ class App extends Component {
           this.setState({ isConnected: true });
         }
       });
-    } else {
-      console.log("No web3? You should consider trying MetaMask!");
-      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-      this.web3 = new Web3(
-        new Web3.providers.HttpProvider("http://localhost:8545")
+    }
+    // Non-dapp browsers...
+    else {
+      console.log(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
       );
     }
   }
--- contracts/contracts/MetaCoin.sol
+++ contracts/contracts/MetaCoin.sol
@@ -8,21 +8,21 @@ import "./ConvertLib.sol";
 // token, see: https://github.com/ConsenSys/Tokens. Cheers!
 
 contract MetaCoin {
-	mapping (address => uint) balances;
+    mapping (address => uint) balances;
 
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-	constructor() public {
-		balances[tx.origin] = 10000;
-	}
+    constructor() public {
+        balances[msg.sender] = 10000;
+    }
 
-	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		emit Transfer(msg.sender, receiver, amount);
-		return true;
-	}
+    function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
+			if (balances[msg.sender] < amount) return false;
+				balances[msg.sender] -= amount;
+				balances[receiver] += amount;
+				emit Transfer(msg.sender, receiver, amount);
+				return true;
+    }
 
 	function getBalanceInEth(address addr) public view returns(uint){
 		return ConvertLib.convert(getBalance(addr),2);
